37c3bd604f21cfcab115f3267aeb7521586ceb2d
SuperTikuwa <tikuwamk2@gmail.com>
2021-03-08 15:48:06 +0900

    openjdk15が動くようになった

--- mariadb/db/init.sql
+++ mariadb/db/init.sql
@@ -1,77 +1,31 @@
 use pre_db;
 
-create table user_parameters(
+create table tasks_list(
     id int auto_increment not null primary key,
-    hp int not null,
-    updated_datetime datetime not null
+    register_day　data not null,
+    detail varchar(128) not null,
+    person_id int not null,
+    deadline data not null,
+    completion int,
+    foreign key (person_id) references person(id) on delete cascade
 );
 
-create table users(
-    id int auto_increment not null primary key,
-    name varchar(128) not null,
-    email varchar(128) not null,
-    password varchar(128) not null,
-    token varchar(128) not null,
-    param_id int not null,
-    foreign key (param_id) references user_parameters(id) on delete cascade
-);
-
-create table weights(
+create table person(
     id int not null primary key,
-    degree varchar(128) not null
-);
-
-create table tasks(
-    id int auto_increment not null primary key,
-    title varchar(128) not null,
-    deadline_date date,
-    deadline_time time,
-    description varchar(128),
-    weight_id int,
-    isAchieve boolean not null,
-    registered_datetime datetime not null,
-    foreign key (weight_id) references weights(id) on delete cascade
-);
-
-create table user_and_task_links(
-    user_id int not null,
-    task_id int not null,
-    foreign key (user_id) references users(id) on delete cascade,
-    foreign key (task_id) references tasks(id) on delete cascade
+    person_name varchar(8)
 );
 
 insert into 
-    weights(id,degree) 
+    person(id,person_name)
 values
-    (1,"ぬるい");
+    (1,"ふくだ");
 
 insert into 
-    weights(id,degree) 
+    person(id,person_name)
 values
-    (2,"ふつう");
+    (2,"かみや");
 
 insert into 
-    weights(id,degree) 
+    person(id,person_name)
 values
-    (3,"えぐい");
-
-insert into
-    user_parameters(hp,updated_datetime)
-values
-    (1000000,Now());
-
-insert into
-    users(id,name,email,password,token,param_id)
-values
-    (1,"Hoge","hoge@hoge.jp","4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d","56f91b5f3668c470912be72ea6cbb0567cfdc0e6ab2266505f3f4b30bab989c6",1);
-
-insert into
-    tasks(title,deadline_date,deadline_time,description,weight_id,isAchieve,registered_datetime)
-values
-    ("睡眠","2020-08-28","21:00:00","ねる",1,false,Now());
-
-insert into
-    user_and_task_links(user_id,task_id)
-values
-    (1,1);
-
+    (3,"にしざわ");
